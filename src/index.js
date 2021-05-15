0649591dad2446a2466c1ee033f2d660dce68cfc
ZobovaA <75171940+ZobovaA@users.noreply.github.com>
2021-05-15 22:22:51 +0300

    switch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import './styles.css';
+import "./styles.css";
 
-import menuTemplate from './templates/menu.hbs'
-import menu from './menu.json'
-const data = {items: menu}
-console.log(data)
-const menuHTML = menuTemplate(data)
-console.log(menuHTML)
-document.querySelector('.js-menu').innerHTML = menuHTML  
+import menuTemplate from "./templates/menu.hbs";
+import menu from "./menu.json";
+const data = { items: menu };
 
+const menuHTML = menuTemplate(data);
 
+document.querySelector(".js-menu").innerHTML = menuHTML;
+
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+
+const switchControl = document.querySelector(".theme-switch__control");
+
+const switchBtn = document.querySelector(".theme-switch__track");
+const checkEl = document.querySelector(".theme-switch__toggle");
+
+const onBtnClickDark = () => {
+  if (document.body.classList.contains("light-theme")) {
+    document.body.classList.remove("light-theme");
+    document.body.classList.add("dark-theme");
+  } else {
+    document.body.classList.add("light-theme");
+    document.body.classList.remove("dark-theme");
+  }
+};
+
+checkEl.addEventListener("click", onBtnClickDark);
