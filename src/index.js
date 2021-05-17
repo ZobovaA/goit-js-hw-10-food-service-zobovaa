219dc83aca0ac0a76152f5faa20c595b32c5b43e
ZobovaA <75171940+ZobovaA@users.noreply.github.com>
2021-05-17 17:37:21 +0300

    3

--- src/index.js
+++ src/index.js
@@ -11,42 +11,22 @@ document.querySelector(".js-menu").innerHTML = menuHTML;
 const Theme = {
   LIGHT: "light-theme",
   DARK: "dark-theme",
-};  
+};
 
+const checkEl = document.querySelector("#theme-switch-toggle");
 
-const checkEl = document.querySelector('#theme-switch-toggle');
-checkEl.addEventListener('change', switchedInput);
-
-function switchedInput(event) {
-  event.currentTarget.checked ? checkedInput() : notCheckedInput();
-}
-
-function checkedInput() {
-  document.body.classList.add('dark-theme');
-  document.body.classList.remove('light-theme');
-  localStorage.setItem('theme', 'dark-theme');
-  checkEl.checked = true;
-}
-
-
-function notCheckedInput() {
-  document.body.classList.add('light-theme');
-  document.body.classList.remove('dark-theme');
-  localStorage.setItem('theme', 'light-theme');
-  checkEl.checked = false;
-}
-
-function currentTheme() {
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'light-theme' || savedTheme === null) {
-    notCheckedInput();
-    return;
-  }
-
-  if (savedTheme === 'dark-theme') {
-    checkedInput();
-    return;
+function onClick(e) {
+  if (checkEl.checked = true) {
+    return localStorage.setItem("theme", "dark-theme");
+   
   }
 }
 
-currentTheme();
+checkEl.addEventListener("click", onClick);
+
+function isBlackTheme() {
+return localStorage.getItem('theme')=== Theme.DARK
+
+}
+
+console.log(isBlackTheme());
